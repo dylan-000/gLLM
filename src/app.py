cc5dda556c759a9ca33c66d886183ebac18ff3cd
Shared <dylanf1954@gmail.com>
2025-11-24 19:41:03 +0000

    Fixed issues with serving content

--- src/app.py
+++ src/app.py
@@ -2,26 +2,19 @@ from openai import AsyncOpenAI
 import chainlit as cl
 
 client = AsyncOpenAI(base_url="http://localhost:8000/v1", api_key="empty")
-cl.instrument_openai();
-
-settings = {
-    "model" : "Qwen3-0.6B",
-    "temperature" : 0.7
-}
+cl.instrument_openai()
+settings = {"model": "gemma-3-12b-it", "temperature": 0.7}
 
 @cl.on_message
-async def on_message(msg : cl.message):
-    response = client.chat.completions.create(
-        messages = [
+async def on_message(msg: cl.Message):
+    response = await client.chat.completions.create(
+        messages=[
             {
-                "content" : "You are a helpful research assistant whose job is to assist with research activities",
-                "role" : "System"
+                "content": "You are a helpful research assistant whose job is to assist with research activities",
+                "role": "system",
             },
-            {
-                "content" : msg.content,
-                "role" : "System"
-            }
+            {"content": msg.content, "role": "user"},
         ],
-        **settings
+        **settings,
     )
     await cl.Message(content=response.choices[0].message.content).send()
