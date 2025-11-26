50555676a6b5fd9ab69572e8f3097d43a30fbd2d
Dylan Fisher <dylanf1954@gmail.com>
2025-11-26 00:55:04 +0000

    got image comprehension to work. Hallelujah

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from openai import AsyncOpenAI
+import base64
 import chainlit as cl
 from PromptManager import PromptManager
 
@@ -9,22 +10,51 @@ pm = PromptManager()
 SYSTEM_PROMPT = pm.getSystem()
 settings = {"model": "gemma-3-12b-it", "temperature": 0.7}
 
+@cl.on_chat_resume
+async def on_chat_resume(thread):
+    pass
+
+
+# TODO: Implment real authentication callback
+@cl.password_auth_callback
+def auth_callback(username: str, password: str):
+    if (username, password) == ("admin", "admin"):
+        return cl.User(
+            identifier="admin", metadata={"role": "admin", "provider": "credentials"}
+        )
+    else:
+        return None
+    
+    
 @cl.on_chat_start
 def on_start():
     cl.user_session.set(
         "message_history",
         [{"content": f'{SYSTEM_PROMPT}', "role": "system"}],
     )
-    print(SYSTEM_PROMPT)
 
-
+# TODO: Add the image from the chainlit "Message" object to the proper input type that the OpenAI API accepts
 @cl.on_message
-async def on_message(msg: cl.Message):
+async def on_message(cl_msg: cl.Message):
+    message = {
+        "role": "user",
+        "content" : [
+            {"type": "text", "text": cl_msg.content}
+        ]
+    }
+    
+    IMAGES = [file for file in cl_msg.elements if "image" in file.mime]
+    
+    for image in IMAGES:
+        with open(image.path, "rb") as image_file:
+            b64_image = base64.b64encode(image_file.read()).decode("utf-8")  
+        message["content"].append({"type": "image_url", "image_url": { "url" : f"data:image/png;base64,{b64_image}" } },)
+        
     message_history = cl.user_session.get("message_history")
-    message_history.append({"content": msg.content, "role": "user"})
+    message_history.append(message)
 
     msg = cl.Message(content="")
-
+    
     stream = await client.chat.completions.create(
         messages=message_history, stream=True, **settings
     )
